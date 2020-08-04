09b32f38822b1860f731e1709018b121b47a349c
Niel Agenbag <niel.agenbag@gmail.com>
2020-08-05 00:21:21 +0200

    Opdateer terugstring van battery vir dekodering

--- toetstheSunPays.py
+++ toetstheSunPays.py
@@ -11,15 +11,17 @@ ser.close()
 from binascii import unhexlify
 
 # Opdrag vanaf paragraaf 5 in seriepoort handleiding
-bytestosend = '7E3235303034363432453030324646464430360D'
+bytestosend = '7E3235303034363432453030323031464433310D'
 
 # Stuur data 1200 baud.  Dit moet eers teen hierdie spoed gestuur word
 with serial.Serial('/dev/ttyUSB0', 9600, timeout=5.0) as ser:
     x = ser.write(unhexlify(bytestosend)) # Stuur opdrag na die battery
-    
     uitstring = ser.read(5000)
+    a = uitstring.hex()
 
 # Druk die greepstring (bytestring)
 print('Greepstring')
 print(uitstring)
+print('Hex string')
+print(a)
 # %%
